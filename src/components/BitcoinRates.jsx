f3bb27ddcbea76345281cc288ad40c4652ae1b82
Ryan24Price <illusion165@hotmail.com>
2024-02-10 11:11:48 -0500

    spell check update

--- src/components/BitcoinRates.jsx
+++ src/components/BitcoinRates.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useBitcoinPrice } from "../hooks/useBitconPrice";
+import { useBitcoinPrice } from "../hooks/useBitcoinPrice";
 import { useMoodContext } from "../Context/MoodContext";
 import Emoji from "./Emoji";
 
